2a11412b4731bdd3b0b7a1ad82a17abb7a3223a1
eagleblastmusic-lgtm <eagleblastmusic@gmail.com>
2026-09-15 04:57:30 +0200

    Mark ePraca official source implemented

--- src/agregator/catalog.py
+++ src/agregator/catalog.py
@@ -8,6 +8,7 @@ DEFAULT_CATALOG_PATH = Path(__file__).resolve().parents[2] / "config" / "source_
 
 IMPLEMENTED_ADAPTERS: dict[int, str] = {
     7: "olx",
+    20: "epraca",
     30: "jooble",
     32: "careerjet",
     79: "adzuna",
