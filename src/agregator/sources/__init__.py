c83795703057402d29793f5ea24fda1b3c21f8a8
eagleblastmusic-lgtm <eagleblastmusic@gmail.com>
2026-09-15 03:17:14 +0200

    feat: expose default source registry

--- src/agregator/sources/__init__.py
+++ src/agregator/sources/__init__.py
@@ -1,1 +1,13 @@
 """Job-source adapters."""
+
+from .olx import OlxPublicSource
+from .registry import SourceRegistry
+
+
+def default_registry() -> SourceRegistry:
+    registry = SourceRegistry()
+    registry.register("olx", OlxPublicSource)
+    return registry
+
+
+__all__ = ["OlxPublicSource", "SourceRegistry", "default_registry"]
