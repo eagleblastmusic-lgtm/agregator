e336c1e6c2fbc746cde065a7416f144c99420f15
eagleblastmusic-lgtm <eagleblastmusic@gmail.com>
2026-09-16 21:07:44 +0200

    Expect OLX to remain selectable in desktop GUI

--- tests/test_gui_app.py
+++ tests/test_gui_app.py
@@ -1,13 +1,15 @@
 from agregator.gui import CREDENTIAL_SOURCES, HOLD_SOURCES, VERIFIED_SOURCES
-from agregator.gui_app import selectable_sources
+from agregator.gui_app import ACTIVE_OLX_SOURCES, BLOCKED_HOLD_SOURCES, selectable_sources
 
 
-def test_safe_gui_never_marks_hold_sources_selectable() -> None:
+def test_gui_keeps_olx_selectable_and_other_hold_sources_blocked() -> None:
     selectable = set(selectable_sources())
 
-    assert selectable == set(VERIFIED_SOURCES + CREDENTIAL_SOURCES)
-    assert not (selectable & set(HOLD_SOURCES))
-    assert "olx" not in selectable
+    assert "olx" in ACTIVE_OLX_SOURCES
+    assert "olx" in selectable
+    assert selectable == set(VERIFIED_SOURCES + ACTIVE_OLX_SOURCES + CREDENTIAL_SOURCES)
+    assert set(BLOCKED_HOLD_SOURCES) == {"pracuj", "theprotocol", "bulldogjob"}
     assert "pracuj" not in selectable
     assert "theprotocol" not in selectable
     assert "bulldogjob" not in selectable
+    assert "olx" in HOLD_SOURCES  # legacy metadata is patched at GUI launch time
